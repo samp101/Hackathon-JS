1ca61b9e104824e1b126012580cddd3d4156ba82
samp101 <shmpos@gmail.com>
2022-07-17 17:10:55 +0300

    update

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 let calContainer = document.querySelector('#calendarContainer')
 let cardsContainer = document.querySelector('#cardsNew')
 let monthHTML = document.querySelector('#date-string')
-
+let dateFormCont = document.querySelector('#dateFormCont')
 
 let table = document.createElement('table')
 let tr = document.createElement('tr')
 
-let month = 7
-let year = 2022
+
+let dateForm = document.createElement('form')
+
+let inputMonth = document.createElement('input')
+inputMonth.classList.add('inputDate')
+inputMonth.type = 'number'
+
+let inputYear = document.createElement('input')
+inputYear.classList.add('inputDate')
+inputYear.type = 'number'
+
+let dateSubmit = document.createElement('input')
+dateSubmit.type = 'submit'
+dateSubmit.value = 'search'
+dateSubmit.classList.add('inputDate')
+
+
+inputMonth.setAttribute('id','inputMonth')
+inputYear.setAttribute('id','inputYear')
+dateSubmit.setAttribute('id','dateSubmit')
+
+dateForm.append(inputMonth,inputYear,dateSubmit)
+console.log(dateForm)
+dateFormCont.append(dateForm)
+
+
+// inputMonth.value = 7
+// inputYear.value = 
+
+let month = inputMonth.value
+let year = inputYear.value
 let date = new Date(year,month-1)
 let dayMonthStart = date.getDay()
+console.log(date)
 let daysInMonth = new Date(year,month,0).getDate()
 
-console.log(date.setDate(date.getDate()+1))
 
 let pTagWithDate = document.createElement('h2')
 
@@ -59,7 +88,9 @@ for (let index = 1; index < daysInMonth+1; index++) {
     plusPng.src = './Static/Plus.png'
 
     let inputContainer = document.createElement('div')
-
+    let subCont = document.createElement('div')
+    let subCont2 = document.createElement('div')
+    
 
     let form = document.createElement('form')
     form.classList.add('form')
@@ -70,6 +101,7 @@ for (let index = 1; index < daysInMonth+1; index++) {
     BdayPng.type = 'image'
     BdayPng.src = './Static/Birthday-cake.png'
     BdayPng.required = true;
+    // subCont.append(BdayPng)
     
     
     let anniversaryPng = document.createElement('input')
@@ -77,6 +109,14 @@ for (let index = 1; index < daysInMonth+1; index++) {
     anniversaryPng.type = 'image'
     anniversaryPng.src = './Static/Growing-heart.png'
     anniversaryPng.required = true;
+    // subCont2.append(anniversaryPng)
+
+    // subCont.style.padding ='10px'
+    // subCont2.style.padding ='10px'
+    // subCont.style.display ='inline-block'
+    // subCont2.style.display ='inline-block'
+    // subCont.classList.add('submit-bday-cont')
+    // subCont2.classList.add('submit-anni-cont')
 
 
     let input = document.createElement('input')
@@ -85,6 +125,7 @@ for (let index = 1; index < daysInMonth+1; index++) {
     input.type = 'text'
 
     form.append(input,BdayPng,anniversaryPng)
+    // form.append(input,subCont,subCont2)
     inputContainer.append(form)
     formSpan.append(plusPng,form)
     td.append(formSpan)
@@ -112,7 +153,6 @@ table.append(tr)
 calContainer.append(table)
 
 
-let userInput = document.querySelector('#userInput6')
 let annivAll = document.querySelectorAll('.b-day-submit')
 let bdayAll = document.querySelectorAll('.anniv-submit')
 let formButton = document.querySelectorAll('.formButton')
@@ -131,31 +171,50 @@ annivAll.forEach(element=>
 function createBdayCard(e) {
     e.preventDefault()
     let specificDay = e.target.parentElement.parentElement.parentElement
-    console.log(specificDay)
-    console.log(e.target)
-
+    
     e.target.setAttribute('required', '') 
 
     let userInfoContainer = document.createElement('div')
     userInfoContainer.classList.add('card')
 
-    let userText = document.createElement('span')
+    let userTextSpan = document.createElement('span')
+    let userInputText = document.createElement('p')
+
+
     
-    userText.textContent = this.parentElement.firstChild.value
+    userInputText.textContent = this.parentElement.firstChild.value
     
+    userTextSpan.append(userInputText)
+    userTextSpan.classList.add('userTextSpan')
     let imgEvent  = document.createElement('img')
     imgEvent.src = this.src
+    
+    let occasionType;
+    if(imgEvent.src == 'http://127.0.0.1:5501/Static/Growing-heart.png'){
+         occasionType = document.createTextNode('Happy Anniversary')
+    }else{
+        occasionType = document.createTextNode('Happy Birthday')
+    }
+    let occasionTypeCont = document.createElement('span')
+    occasionTypeCont.classList.add('event-status')
+    occasionTypeCont.append(occasionType)
+
+
 
     let deleteCard = document.createElement('img')
     deleteCard.src ='./Static/Delete.png'
     deleteCard.addEventListener('click',remove_div)
-    
-    this.parentElement.firstChild.value = ''
-    let formDiv = this.parentElement
-    formDiv.style.visibility = 'hidden' 
+
 
     
-    userInfoContainer.append(imgEvent,userText,deleteCard)
+    this.parentElement.firstChild.value = ''
+    let imgOpen = this.parentElement.parentElement.firstChild
+    console.log(imgOpen)
+    let formDiv = this.parentElement
+    formDiv.style.visibility = 'hidden' 
+    imgOpen.style.visibility = ''
+ 
+    userInfoContainer.append(imgEvent,occasionTypeCont,userTextSpan,deleteCard)
     specificDay.append(userInfoContainer)
 }
 
@@ -187,13 +246,18 @@ showForm.forEach(element=>{
     element.addEventListener('click',reveal)})
 
 function reveal(element){
+
     
         if(element.target.nextElementSibling.style.visibility == 'visible'){
-            // element.target.style.visibility = 'hidden'
+            element.target.style.visibility = ''
             element.target.nextElementSibling.style.visibility ='hidden' 
+            element.target.style.visibility = ''
         } else {element.target.nextElementSibling.style.visibility == 'hidden'
         // element.target.style.visibility = 'visible'
-            element.target.nextElementSibling.style.visibility  = 'visible'}
+            element.target.nextElementSibling.style.visibility  = 'visible'
+            element.target.style.visibility = 'visible'
+        }
+
     }
 
 document.addEventListener('click', function handleClickOutsideBox(event) {
